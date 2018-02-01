c18d042ae3b480f3f6339270d51f3e30e9985db0
jim <jim.nguyen03@gmail.com>
2018-02-01 10:09:19 -0800

    fixed cors issue

--- server.js
+++ server.js
@@ -14,25 +14,20 @@ const db = knex({
   client: 'pg',
   connection: {
     connectionString : process.env.DATABASE_URL,
-    ssl: true,
+    ssl: true
   }
 });
 
 const app = express();
 
-app.use(cors());
+app.use(cors())
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => { res.send('it is working') })
-
-app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
-
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt)})
-
+app.get('/', (req, res)=> { res.send('it is working') })
+app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
 app.listen(process.env.PORT || 3000, () => {
